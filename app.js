d081f3c2ac0c978b436606c606b6ed65304117ad
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2021-03-18 21:56:06 -0400

    green underline added to language guess name

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const franc = require('franc');
 const langs = require('langs');
+const colors = require('colors');
 const input = process.argv[2];
 
 try {
     const langCode = franc(input, {minLength: 3})
     const languageName = langs.where("3",langCode)
-    console.log(languageName.name)
+    console.log(`Our guess: ${languageName.name.underline.green}`)
 } catch (e) {
     console.log(e)
     console.log('Too short of a string. Try longer than 3 characters')
