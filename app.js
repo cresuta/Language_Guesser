a3b3a92a68164afcff3ddaa3d2a6c9f763de2725
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2021-03-18 21:24:54 -0400

    removing npm prompt pkg, franc code converted to langs name

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const prompt = require('prompt-sync')();
 const franc = require('franc');
-const userInput = prompt('Type a word or sentence and I will guess the language:')
-console.log(franc(userInput))
+const langs = require('langs');
+
+const langCode = franc('Alle menslike wesens word vry')
+console.log(langs.where("3", langCode).name);
