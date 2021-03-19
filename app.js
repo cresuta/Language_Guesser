fd31e44aae0a38fd0c5f4aeddb4a391cd0211134
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2021-03-18 21:48:15 -0400

    try/catch with minLength franc input of 3

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const franc = require('franc');
 const langs = require('langs');
 const input = process.argv[2];
-const langCode = franc(input)
-const languageName = langs.where("3",langCode)
 
-console.log(languageName.name)
+try {
+    const langCode = franc(input, {minLength: 3})
+    const languageName = langs.where("3",langCode)
+    console.log(languageName.name)
+} catch (e) {
+    console.log(e)
+    console.log('Too short of a string. Try longer than 3 characters')
+}
